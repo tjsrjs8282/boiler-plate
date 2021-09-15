c000aa67f8f36e0ab0ebf75ee7e25af93f7778f4
seongeon <dbtjsrjs8282@naver.com>
2021-09-16 01:52:56 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 //express 앱을 만들다
-const app = express()
-const port = 3000
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const config = require('./config/key');
 
+const {User} = require("./models/User");
  
-mongoose.connect('mongodb+srv://seongeon:tlqkfsha2!@cluster0.20joh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+//application / x-xxx-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}))
+//application / json
+app.use(bodyParser.json());
+
+
+mongoose.connect('config.mongoURI',{
    //에러안뜨게 4가지 추가
    useNewUrlParser:true, useUnifiedTopology:true
  }).then(()=> console.log('잘들어감zz')).catch(err=>console.log(err))
@@ -15,7 +24,23 @@ mongoose.connect('mongodb+srv://seongeon:tlqkfsha2!@cluster0.20joh.mongodb.net/m
 
 //불러오면 핼로월드 글자
 app.get('/', (req, res) => {
-  res.send('Hello World! 백앤드 가주아잉!')
+  res.send('Hello World! 백앤드 가주아잉!!!!!!!')
+})
+
+app.post('/register', (req, res) => {
+
+  //회원가입시 필요한 정보들을 client 에서 가여오면 그것들을 데이터베이스에 넣어준다.
+
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo)=>{
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+
 })
 
 //리슨을하면 터미널에 문자로그
--- models/User.js
+++ models/User.js
@@ -16,7 +16,7 @@ const userSchema = mongoose.Schema({
     },
     lastname:{
         type:String,
-        maxlength50
+        maxlength:50
     },
     role:{
         type:Number,
